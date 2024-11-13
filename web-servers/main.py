ab019bcfb0f889171557c5d15491a0bdcb5b1f73
j4carlos <j4carlos@gmail.com>
2024-11-13 07:55:52 -0300

    Se agrego fastapi y modifico la webapp para tener un server

--- web-servers/main.py
+++ web-servers/main.py
@@ -1,4 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <h1>Hola Mundo!</h1>
+    <video  width="640" height="360" controls>
+        <source src="https://i.imgur.com/B6QKKcv.mp4" type="video/mp4">
+    </video>
+    """
+    #{'name': 'Carlos'}
 
 def run():
     store.get_categories()
