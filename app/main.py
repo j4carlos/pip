6d8ab94576d49d04abf272d66dbc8f71372a31b6
j4carlos <j4carlos@gmail.com>
2024-11-15 00:48:35 -0300

    Agregado graficos de burbujas y menu de opciones

--- app/main.py
+++ app/main.py
@@ -11,20 +11,24 @@ def run():
   porcentaje = list(map(lambda x: x['World Population Percentage'],data))
   '''
   df = pd.read_csv('data.csv')
-  
-  df = df[df['Continent'] == 'Europe']
-  paises = df['Country/Territory'].values
-  porcentaje = df['World Population Percentage'].values
-  charts.generate_pie_chart(paises,porcentaje)
-  
-  data = read_csv.read_csv('data.csv')
-  country = input ('Type Country : ')
-  resultado = utilidades.population_by_country(data,country)
+  choice = input('Type option: \n\t1 - Continent population \n\t2 - Country population\n==> ')
+  if choice == '1':
+    continent = input('Type continent (Asia,Africa,Europe,North America,South America,Oceania): ')
+    df = df[df['Continent'] == continent.title()]
+    paises = df['Country/Territory'].values
+    porcentaje = df['World Population Percentage'].values
+    charts.generate_pie_chart(paises,porcentaje)
+    charts.generate_bubble(continent,paises,porcentaje)
+    print('Archivos generados en bubble.png y pie.png con exito')
+  else:
+    data = read_csv.read_csv('data.csv')
+    country = input ('Type Country : ')
+    resultado = utilidades.population_by_country(data,country)
 
-  if len(resultado) > 0:
-    country_bar = resultado[0]
-    labels, values = utilidades.get_population(country_bar)
-    charts.generate_bar_chart(labels,values,country)
+    if len(resultado) > 0:
+      country_bar = resultado[0]
+      labels, values = utilidades.get_population(country_bar)
+      charts.generate_bar_chart(labels,values,country)
 
 if __name__ == '__main__':
   run()
